7bf415ec8018f0adb7cbc885c009c5e05db44176
ANTIMA-BHARDWAJ <162999970+ANTIMA-BHARDWAJ@users.noreply.github.com>
2025-03-10 01:25:18 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000; // You can change this port
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
