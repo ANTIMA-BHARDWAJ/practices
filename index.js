e361e1c944ef65782e72ff01aead3633c531cf49
ANTIMA-BHARDWAJ <162999970+ANTIMA-BHARDWAJ@users.noreply.github.com>
2025-03-12 09:23:19 -0400

    Update index.js
    code extend

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ import express from 'express';
 const app = express();
 const PORT = 3000; // You can change this port
 
-// Define a route
+/*// Define a route
 app.get('/', (req, res) => {
-  res.sendFile("templates/index.html",{root:__dirname});
-});
+  res.sendFile("index.html" {root:"C:\\Users\\LENOVO\\Desktop\\New folder\\templates"});
+}); */
+
+app.get('/', (req, res) => {
+  let siteName = "Adidas"
+  let searchText = "Search Now"
+  let arr = ["Hey", 54, 65]
+  res.render("index", { siteName: siteName, searchText: searchText, arr })
+})
+
+app.get('/blog/:slug', (req, res) => {
+  let blogTitle = "Adidas why and when?"
+  let blogContent = "Its a very good brand"
+  res.render("blogpost", {blogTitle: blogTitle, blogContent: blogContent})
+})
+
 
 // Start the server
 app.listen(PORT, () => {
