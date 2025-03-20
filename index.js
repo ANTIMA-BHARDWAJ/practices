fe18f30ebf7495705a0628ae405b3712f7f89c92
ANTIMA-BHARDWAJ <162999970+ANTIMA-BHARDWAJ@users.noreply.github.com>
2025-03-20 04:20:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+import mongoose from 'mongoose';
 import express from 'express';
+import { Data } from "./models/data.js";  // Ensure filename matches
 
+let conn = await mongoose.connect("mongodb://localhost:27017/data")
 const app = express();
-const PORT = 3000; // You can change this port
+const PORT = 3005; // You can change this port
 
-app.set("view engine", "ejs");
+//app.set("view engine", "ejs");
 
 /*// Define a route
 app.get('/', (req, res) => {
   res.sendFile("index.html" {root:"C:\\Users\\LENOVO\\Desktop\\New folder\\templates"});
-}); */
+}); 
 
 app.get('/', (req, res) => {
   let siteName = "Adidas"
@@ -21,8 +24,14 @@ app.get('/blog/:slug', (req, res) => {
   let blogTitle = "Adidas why and when?"
   let blogContent = "Its a very good brand"
   res.render("blogpost", {blogTitle: blogTitle, blogContent: blogContent})
-})
+}) */
 
+app.get('/',(req, res) => {
+
+  const data = new Data({name:"HARRY",course:"python",isManger:true})
+  data.save()
+  res.send('Hello World!')
+})
 
 // Start the server
 app.listen(PORT, () => {
